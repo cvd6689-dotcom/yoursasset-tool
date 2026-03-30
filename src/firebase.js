123ea0d5feedaafd2ddfba1aa02203464b83b351
cvd6689-dotcom <cvd6689@gmail.com>
2026-03-30 17:45:44 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "여기에복붙",
-  authDomain: "여기에복붙",
-  projectId: "여기에복붙",
-  storageBucket: "여기에복붙",
-  messagingSenderId: "여기에복붙",
-  appId: "여기에복붙",
-  measurementId: "여기에복붙"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
