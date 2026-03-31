d8dff2c255a50396dbb1b0255b24d1224606a5ae
cvd6689-dotcom <cvd6689@gmail.com>
2026-03-31 15:54:18 +0900

    Update insurers.js

--- src/data/insurers.js
+++ src/data/insurers.js
@@ -1,36 +1,46 @@
-const INSURER_MASTER = [
+const insurers = [
   {
     id: "samsung",
     name: "삼성화재",
     logo: "/insurers/samsung.png",
-    color: "#1f6feb",
-    order: 1,
-    useYn: true,
+    strengths: ["건강보험", "암/뇌/심장", "운전자", "가족보장"],
+    tags: ["브랜드신뢰", "담보구성", "플랜안정감"],
   },
   {
     id: "db",
     name: "DB손해보험",
     logo: "/insurers/db.png",
-    color: "#1c8c4c",
-    order: 2,
-    useYn: true,
+    strengths: ["건강보험", "운전자", "간병", "종합보장"],
+    tags: ["가성비", "담보폭", "실속형"],
   },
   {
     id: "hyundai",
     name: "현대해상",
     logo: "/insurers/hyundai.png",
-    color: "#ef7d00",
-    order: 3,
-    useYn: true,
+    strengths: ["어린이", "가족", "건강보험", "운전자"],
+    tags: ["가족형", "인지도", "밸런스형"],
   },
   {
     id: "heungkuk",
     name: "흥국화재",
     logo: "/insurers/heungkuk.png",
-    color: "#d91c5c",
-    order: 4,
-    useYn: true,
+    strengths: ["유병자", "건강보험", "간편심사", "실속보장"],
+    tags: ["유연성", "실속형", "간편플랜"],
+  },
+  {
+    id: "kb",
+    name: "KB손해보험",
+    logo: "/insurers/kb.png",
+    strengths: ["운전자", "건강보험", "가족보장", "생활밀착보장"],
+    tags: ["대중성", "생활형", "운전자"],
+  },
+  {
+    id: "meritz",
+    name: "메리츠화재",
+    logo: "/insurers/meritz.png",
+    strengths: ["건강보험", "암/수술", "실속플랜", "종합보장"],
+    tags: ["실무활용", "집중보장", "실속형"],
   },
 ];
 
-export default INSURER_MASTER;
+export default insurers;
