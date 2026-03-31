f0e83dfc94fa749db7e18fe27f6c1d510ffaa80a
cvd6689-dotcom <cvd6689@gmail.com>
2026-03-31 14:56:50 +0900

    Update insurers.js

--- src/data/insurers.js
+++ src/data/insurers.js
@@ -4,24 +4,32 @@ const INSURER_MASTER = [
     name: "삼성화재",
     logo: "/insurers/samsung.png",
     color: "#1f6feb",
+    order: 1,
+    useYn: true,
   },
   {
     id: "db",
     name: "DB손해보험",
     logo: "/insurers/db.png",
     color: "#1c8c4c",
+    order: 2,
+    useYn: true,
   },
   {
     id: "hyundai",
     name: "현대해상",
     logo: "/insurers/hyundai.png",
     color: "#ef7d00",
+    order: 3,
+    useYn: true,
   },
   {
     id: "heungkuk",
     name: "흥국화재",
     logo: "/insurers/heungkuk.png",
     color: "#d91c5c",
+    order: 4,
+    useYn: true,
   },
 ];
 
