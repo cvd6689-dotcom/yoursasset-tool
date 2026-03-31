3c51a3d21e0a26e2d2405aa3219bf5adc41c7aa5
cvd6689-dotcom <cvd6689@gmail.com>
2026-03-31 14:17:55 +0900

    Create insurers.js

---
+++ src/data/insurers.js
@@ -0,0 +1,28 @@
+const INSURER_MASTER = [
+  {
+    id: "samsung",
+    name: "삼성화재",
+    logo: "/insurers/samsung.png",
+    color: "#1f6feb",
+  },
+  {
+    id: "db",
+    name: "DB손해보험",
+    logo: "/insurers/db.png",
+    color: "#1c8c4c",
+  },
+  {
+    id: "hyundai",
+    name: "현대해상",
+    logo: "/insurers/hyundai.png",
+    color: "#ef7d00",
+  },
+  {
+    id: "heungkuk",
+    name: "흥국화재",
+    logo: "/insurers/heungkuk.png",
+    color: "#d91c5c",
+  },
+];
+
+export default INSURER_MASTER;
